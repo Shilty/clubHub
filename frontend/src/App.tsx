3ba4000dd07cdeac7ebb3a5e60a7f6b1d475df6e
Vasile Lazar <vlazar184@gmail.com>
2026-02-02 16:04:23 +0200

    Created frontend and backend templates
    React+typescript frontend and ASP.NET backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        fontSize: '3rem',
+        fontWeight: 'bold'
+      }}>
+        WIP
+      </div>
+  );
+}
+
+export default App;
