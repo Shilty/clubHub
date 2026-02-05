2b65a55d7d13feec288f0f01e68fffd395522fb5
Vasile Lazar <64280721+vasile-lazar@users.noreply.github.com>
2026-02-05 10:43:21 +0200

    Delete backend directory

--- backend/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; } = null!;
-        public string Role { get; set; } = "Student"; // Student, ClubAdmin, PlatformAdmin
-    }
-}
