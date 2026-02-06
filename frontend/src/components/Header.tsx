5eaee83e0c6a65d2ddeb5ac473d5b409e7eb0211
Vasile Lazar <vlazar184@gmail.com>
2026-02-06 14:22:34 +0200

    Added log-in sing-up buttons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,35 +1,25 @@
 ﻿import React from 'react';
 import { Link } from "react-router-dom";
 import '../styles/main.css';
-interface SocialIcon {
-    href: string;
-    label: string;
-    iconClass: string;
-}
 
 const Header: React.FC = () => {
-    const socialLinks: SocialIcon[] = [
-        { href: "#", label: "Twitter", iconClass: "fa-twitter" },
-        { href: "#", label: "Facebook", iconClass: "fa-facebook-f" },
-        { href: "#", label: "Snapchat", iconClass: "fa-snapchat-ghost" },
-        { href: "#", label: "Instagram", iconClass: "fa-instagram" },
-        { href: "#", label: "Medium", iconClass: "fa-medium-m" },
-    ];
-
     return (
         <header id="header">
             <Link to="/" className="logo">
                 <strong>ClubHub</strong>
             </Link>
 
-            <ul className="icons">
-                {socialLinks.map((social, index) => (
-                    <li key={index}>
-                        <Link to={social.href} className={`icon brands ${social.iconClass}`}>
-                            <span className="label">{social.label}</span>
-                        </Link>
-                    </li>
-                ))}
+            <ul className="auth-links">
+                <li>
+                    <Link to="/login" className="login-link">
+                        Log In
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/signup" className="signup-link">
+                        Sign Up
+                    </Link>
+                </li>
             </ul>
         </header>
     );
